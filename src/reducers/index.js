2846f36b700e5b2ffe2de179ab95d693447bdc63
Cha-dieleman <chadieleman@gmail.com>
2019-12-18 16:12:51 +0100

    conf combine_reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@
 import {combineReducers} from 'redux';
 
 const rootReducer = combineReducers({
-    cléSouhaitée : reducerSouhaité,
-    cléSouhaitée : reducerSouhaité,
+    // cléSouhaitée : reducerSouhaité,
+    // cléSouhaitée : reducerSouhaité,
 });
 
