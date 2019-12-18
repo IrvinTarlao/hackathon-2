05955804533d9c8bf21dd89bea150bdd15463a19
irvintarlao <irvin.tarlao@gmail.com>
2019-12-18 21:20:27 +0100

    map dans search2 ok, plus qu'a récupérer le composant pour l'intégrer dans Search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import LandingPage from "./containers/LandingPage";
+import Search2 from "./containers/Search2.js"
+
 // import Search from './containers/Home';
 // import Activity from './containers/Home';
 // import Cart from './containers/Home';
@@ -12,10 +14,10 @@ function App() {
       <HashRouter>
         <Switch>
           <Route exact path="/" component={LandingPage}></Route>
-          {/* <Route path="/search" component={Search}></Route>
-          <Route path="/activity" component={Activity}></Route>
+          <Route path="/search2" component={Search2}></Route>
+          {/* <Route path="/activity" component={Activity}></Route>
           <Route path="/cart" component={Cart}></Route>
-          <Route path="/newactivity" component={NewActivity}></Route> */} */}
+          <Route path="/newactivity" component={NewActivity}></Route> */}
         </Switch> 
      </HashRouter>
     </div>
