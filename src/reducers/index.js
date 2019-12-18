c9cf1b52eca676eb88740fbec5876bc7ba21a000
Cha-dieleman <chadieleman@gmail.com>
2019-12-18 16:10:19 +0100

    config combine

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+// penser à importer les reducers;
+
+import {combineReducers} from 'redux';
+
+const rootReducer = combineReducers({
+    cléSouhaitée : reducerSouhaité,
+    cléSouhaitée : reducerSouhaité,
+});
+
