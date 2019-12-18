b7051808ad9d17914683728b1f82ea9e91a04bc6
Axel Bizel <axel.bizel@gmail.com>
2019-12-18 17:57:42 +0100

    section examples ok

---
+++ src/containers/LandingPage.js
@@ -0,0 +1,125 @@
+import React, { Component } from "react";
+import "../App.css";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText
+} from "reactstrap";
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div className="landingPage">
+        <div className="lpsection-home">
+          <Container>
+            <Row>
+              <Col>
+                <InputGroup>
+                  <Input />
+                  <InputGroupAddon addonType="append">
+                    <InputGroupText>To the Right!</InputGroupText>
+                  </InputGroupAddon>
+                </InputGroup>
+              </Col>
+              <Col>
+                <h1>KidDo</h1>
+                <h3>Mon cadeau ? C'est moi qui l'a fé !</h3>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+        <div className="lpsection-examples">
+          <h2>Quelques exemples</h2>
+          <Container>
+            <Row>
+              <Col xs="12" md="4">
+                <div className="card-item">
+                  <Card>
+                    <CardImg
+                      top
+                      width="100%"
+                      src="https://www.maman-plume.fr/wp-content/uploads/2019/04/shoot-7112-1080x720.jpg"
+                      alt="Card image cap"
+                    />
+                    <CardBody>
+                      <CardTitle>Activité</CardTitle>
+                      <CardSubtitle>Age - Ville - Catégorie</CardSubtitle>
+                      <CardText>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </CardText>
+                      <Button>En savoir plus</Button>
+                    </CardBody>
+                  </Card>
+                </div>
+              </Col>
+              <Col xs="12" md="4">
+                <div className="card-item">
+                  <Card>
+                    <CardImg
+                      top
+                      width="100%"
+                      src="https://www.maman-plume.fr/wp-content/uploads/2019/04/shoot-7112-1080x720.jpg"
+                      alt="Card image cap"
+                    />
+                    <CardBody>
+                      <CardTitle>Activité</CardTitle>
+                      <CardSubtitle>Age - Ville - Catégorie</CardSubtitle>
+                      <CardText>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </CardText>
+                      <Button>En savoir plus</Button>
+                    </CardBody>
+                  </Card>
+                </div>
+              </Col>
+              <Col xs="12" md="4">
+                <div className="card-item">
+                  <Card>
+                    <CardImg
+                      top
+                      width="100%"
+                      src="https://www.maman-plume.fr/wp-content/uploads/2019/04/shoot-7112-1080x720.jpg"
+                      alt="Card image cap"
+                    />
+                    <CardBody>
+                      <CardTitle>Activité</CardTitle>
+                      <CardSubtitle>Age - Ville - Catégorie</CardSubtitle>
+                      <CardText>
+                        Some quick example text to build on the card title and
+                        make up the bulk of the card's content.
+                      </CardText>
+                      <Button>En savoir plus</Button>
+                    </CardBody>
+                  </Card>
+                </div>
+              </Col>
+
+
+            </Row>
+          </Container>
+        </div>
+
+        <div className="lpsection">
+          <h1>KidDo</h1>
+          <h2>Tu veux un cadeau ?</h2>
+          <h2>Fais-le ! (connard.sse)</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
