eba1f16e632d37f6c900870e88a193321e2786fe
irvintarlao <irvin.tarlao@gmail.com>
2019-12-19 13:46:19 +0100

    logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,6 @@ import {
   Collapse,
   Navbar,
   NavbarToggler,
-  NavbarBrand,
   Nav,
   NavItem,
   NavLink as Li,
@@ -19,14 +18,18 @@ const Nav2 = (props) => {
 
   return (
     <div>
+        <h1 className='main-title' style={{position:"fixed", fontSize: "3em", left:"15px"}}>
+          <NavLink className="logo" to={"/"}>KidDo</NavLink>
+        </h1>
         <Navbar className="nav" light expand="md" style={{backgroundColor:"transparent", position: "fixed", right:0, justifyContent:"flex-end", outline:"none", zIndex:1}}>
         
-        <NavbarToggler onClick={toggle} style={{border: "2px solid white", right:"0", backgroundColor:"lightblue"}}/>
+        <NavbarToggler onClick={toggle} style={{border: "2px solid white", right:"0", backgroundColor:"#5BD2FF"}}/>
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar >
             <NavItem >
-              <Li style={{color: "white", textAlign:"right", backgroundColor:"rgba(0, 0, 0, 0.5)"}}>
-              <NavLink className="navlink" to={"/Search"}>notre concept</NavLink></Li>
+              <Li style={{color: "white", textAlign:"right", backgroundColor:"rgba(91, 210, 255, 0.8)"}}>
+                <NavLink className="navlink2" to={"/Search"}>notre concept</NavLink>
+              </Li>
             </NavItem>            
             <NavItem >
               <Li style={{color: "white", textAlign:"right", backgroundColor:"rgba(0, 0, 0, 0.5)"}}>
