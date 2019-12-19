117ab407e5d2ce3bc75064459b89c3e28ebcfc08
Cha-dieleman <chadieleman@gmail.com>
2019-12-19 20:47:44 +0100

    fin mise en page modalActivity

--- src/containers/ModalActivity.js
+++ src/containers/ModalActivity.js
@@ -4,6 +4,14 @@ import {
     Button,
     Jumbotron,
 } from "reactstrap";
+import {
+    CardText,
+  } from "reactstrap";
+  import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+  import { faHome } from "@fortawesome/free-solid-svg-icons";
+  import { faClock } from "@fortawesome/free-solid-svg-icons";
+  import { faChild } from "@fortawesome/free-solid-svg-icons";
+  import { faCoins } from "@fortawesome/free-solid-svg-icons";
 import {connect} from 'react-redux';
 import {isSelected} from '../actions';
 
@@ -35,12 +43,21 @@ class ModalActivity extends Component {
                         </div>
                         <div className="flex">
                             <img src={activity.activity_picture} className="modalActivity_img"></img>
-                            <Jumbotron className="modalActivity_jumbotron">
-                                <h4 className="display-3">{activity.activity_title}</h4>
-                                <h5>Cette activité se situe à {activity.location}</h5>
-                                <p>Lors de cet atelier avec {activity.first_name} {activity.last_name} de {activity.activity_duration} heure(s), vous aurez l'occasion de passer un moment en famille tout en vous cultivant. Pour pouvoir profiter pleinement de ce moment, cette activités vous est conseillée pour des enfants âgés de {activity.activity_age_min} à {activity.activity_age_max} ans.
-                                </p>
-                            </Jumbotron>
+                            <div>
+                                <Jumbotron className="modalActivity_jumbotron">
+                                    <h4 className="display-3">{activity.activity_title}</h4>
+                            
+                                    <h5>Cette activité se situe à {activity.location}</h5>
+                                    <p>Lors de cet atelier avec {activity.first_name} {activity.last_name} de {activity.activity_duration} heure(s), vous aurez l'occasion de passer un moment en famille tout en vous cultivant. Pour pouvoir profiter pleinement de ce moment, cette activités vous est conseillée pour des enfants âgés de {activity.activity_age_min} à {activity.activity_age_max} ans.
+                                    </p>
+                                    <br />
+                                    <br />
+                                    <CardText>
+                                        <FontAwesomeIcon icon={faHome} className="espacer_icones" />&nbsp;{activity.location}<FontAwesomeIcon icon={faClock} className="espacer_icones" />&nbsp;{activity.activity_duration} heure(s)<FontAwesomeIcon icon={faChild} className="espacer_icones" />&nbsp;{activity.activity_age_min} -{" "}{activity.activity_age_max} ans<FontAwesomeIcon icon={faCoins} className="espacer_icones" />&nbsp;{activity.price} euros
+                                    </CardText>
+                                    <br />
+                                </Jumbotron>
+                            </div>
                         </div>
                         <Button color="primary" size="lg" style={{marginTop: "2vh"}} onClick={this.selectedActivityAndCloseModal}>Sélectionner cette activité</Button>
                     </aside> : null
